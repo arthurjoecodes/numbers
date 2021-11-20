7dcaf42294089634aadbd097d9f7fa5ca36543d9
arthurjoecodes <artniel23@gmail.com>
2021-11-20 18:51:01 +0300

    modified files

---
+++ numbers.py
@@ -0,0 +1,15 @@
+# pi = 3.14159
+# print(pi)
+
+# first_num = 153
+# second_num = 68
+# print(first_num + second_num)
+
+
+
+# pi = 3.14159
+# print(pi)
+
+first_num = input('please enter a number')
+second_num = input('please enter another number')
+print(int(first_num) + int(second_num))
